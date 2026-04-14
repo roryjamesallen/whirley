cd1a97ca53b515732b62ee572b2fcb48675f629a
roryjamesallen <rory.james.allen1@gmail.com>
2026-04-14 15:21:53 +0100

    add comments and remove error loggin

--- admin/index.php
+++ admin/index.php
@@ -1,69 +1,64 @@
 <?php
+$blog_url = 'https://yoursite.com/blog';
+$password_hash = 'passwordhash';
 
-$blog_url = 'blog';
-$password_hash = '$2y$10$ZSHhR1CkeXoifijLc1fy3uUZOicCxnR8NJhsewwFdFrslFULpdpSy';
+session_start(); // Session data is used to store whether the user is logged in or not
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-session_start();
-
-function renderMessages(){
+function renderMessages(){ // Render error or success messages if present
     $error = $message = '';
-    if (isset($_GET["error"])){
-	$error = $_GET["error"];
-    } else if (isset($_GET["message"])){
-	$message = $_GET["message"];
+    if (isset($_GET["error"])){ // An error message was passed as a GET parameter
+        $error = $_GET["error"];
+    } else if (isset($_GET["message"])){ // A success or other message was passed as a GET parameter
+        $message = $_GET["message"];
     }
     return '<span class="error">'.$error.'</span>
-    <span class="success-message">'.$message.'</span>'; // Will have display: none set if empty
+    <span class="success-message">'.$message.'</span>'; // Display the error and/or success message(s)
 }
 function checkFieldsFilled($input_names){ // For an array of input names, check if the POST data was filled and not blank
     foreach ($input_names as $input_name){ // For every input name in the array
         if (!isset($_POST[$input_name]) or $_POST[$input_name] == ''){ // If the input wasn't set or was blank
             header("Location: ?error=Fill+in+all+fields"); // Refresh with error message at the first failure
-	    die(); // Prevent further PHP processing
+            die(); // Prevent further PHP processing
         }
     }
 }
 function convertArticleToXML(){
     global $blog_url;
-    $article_xml = "<item><link>{$blog_url}?article={$_POST['link']}</link>";
-    foreach(['title','description','pubDate'] as $tag){
-	$content = htmlspecialchars($_POST[$tag], ENT_QUOTES); // Make sure there's no HTML content or other weirdness
+    $article_xml = "<item><link>{$blog_url}?article={$_POST['link']}</link>"; // Generate the absolute link in XML form
+    foreach(['title','description','pubDate'] as $tag){ // For each tag within the XML <item>
+        $content = htmlspecialchars($_POST[$tag], ENT_QUOTES); // Make sure there's no HTML content or other weirdness
         $article_xml .= "<{$tag}>{$content}</{$tag}>"; // Use the input names as XML tag names
     }
     $article_xml .= '</item>';
     return $article_xml;
 }
 function submitArticle(){
-    $xml = file_get_contents('../rss.xml');
-    $article_xml = convertArticleToXML();
+    $xml = file_get_contents('../rss.xml'); // Get the raw RSS feed
+    $article_xml = convertArticleToXML(); // Convert the POST article parameters into XML
     $new_post_position = strpos($xml, '<item>'); // Insert before the first (newest) item already there
-    if ($new_post_position == 0){ // No items yet, insert before end of channel
-	$new_post_position = strpos($xml, '</channel>');
+    if ($new_post_position == 0){ // If there are no existing items in the feed
+        $new_post_position = strpos($xml, '</channel>'); // Insert the new (first) item before the end of the channel
     }
     $xml = substr_replace($xml, convertArticleToXML(), $new_post_position, 0); // Insert the new post into the XML
-    file_put_contents('../rss.xml', $xml);
-    header("Location: ?message=Post+submitted+successfully");
+    file_put_contents('../rss.xml', $xml); // Write the new feed back to the file
+    header("Location: ?message=Post+submitted+successfully"); // GET redirect to prevent form resubmission
 }
 
 $show_login = true;
 if (isset($_SESSION['logged_in'])){ // Already logged in
     $show_login = false;
-    if (isset($_POST['submit'])){
-	checkFieldsFilled(['title','description','link']);
-	submitArticle();
+    if (isset($_POST['submit'])){ // If the user has clicked submit to create a new post
+        checkFieldsFilled(['title','description','link']); // Make sure all HTML form fields are filled
+        submitArticle(); // Submit the new article content
     }
 } else if (isset($_POST['login'])){ // Pressed login but not yet successful
-    checkFieldsFilled(['password']);
+    checkFieldsFilled(['password']); // Make sure the password field isn't blank
     global $password_hash;
-    if (password_verify($_POST['password'], $password_hash)){
-	$_SESSION['logged_in'] = true;
-	header("Location: ?message=Logged+in+successfully");
+    if (password_verify($_POST['password'], $password_hash)){ // Check the filled in password is correct
+        $_SESSION['logged_in'] = true; // Setting true means that login form won't be shown on page reload
+        header("Location: ?message=Logged+in+successfully"); // GET redirect to prevent resubmission
     } else {
-	header("Location: ?error=Incorrect+password");
+        header("Location: ?error=Incorrect+password"); // GET redirect to prevent resubmission
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,63 +1,59 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 function getFeedAsJSON(){
     $feed = array('title'=>'Blog Title','articles'=>[]); // Default values prevent errors when feed is empty
     $xml = file_get_contents('rss.xml'); // Read the XML file containing the RSS feed itself
     $articles_regex = '/<item>[\s\S]*?<link>(?<link>.*?)<\/link>[\s\S]*?<title>(?<title>[\s\S]*?)<\/title>[\s\S]*?<description>(?<description>[\s\S]*?)<\/description>[\s\S]*?<pubDate>(?<date>.*?)<\/pubDate>[\s\S]*?<\/item>/';
     $title_regex = '/<channel>[\s\S]*?<title>(?<title>.*?)<\/title>[\s\S]*?<\/channel>/';
     foreach (array('articles'=>$articles_regex,'title'=>$title_regex) as $key=>$regex){
-	if (preg_match_all($regex, $xml, $matches, PREG_SET_ORDER)){ // As long as at least one match is found
-	    $feed[$key] = $matches; // Array of objects, each object has a 'title', 'link', and 'description' key=>value pair
-	}
+        if (preg_match_all($regex, $xml, $matches, PREG_SET_ORDER)){ // As long as at least one match is found
+            $feed[$key] = $matches; // Array of objects, each object has a 'title', 'link', and 'description' key=>value pair
+        }
     }
     return $feed;
 }
-function getHandleByLink($link){
-    $url_elements = explode('=', $link);
-    return $url_elements[count($url_elements)-1];
+function getHandleByLink($link){ // Get the handle (unique identifier) of an article from its absolute URL
+    $url_elements = explode('=', $link); // Split using '=' as the separator
+    return $url_elements[count($url_elements)-1]; // Return the last item (after the last '=')
 }
-function getArticleByHandle($handle){
-    $feed = getFeedAsJSON();
-    foreach ($feed['articles'] as $article){
-	if ($handle == getHandleByLink($article['link'])){
-	    return $article;
-	}
+function getArticleByHandle($handle){ // Get an article in JSON format by its handle (unique identifier)
+    $feed = getFeedAsJSON(); // Get the entire XML feed in JSON format
+    foreach ($feed['articles'] as $article){ // For every article in the feed
+        if ($handle == getHandleByLink($article['link'])){ // If the article's handle is the one passed
+            return $article; // Return the article in JSON format
+        }
     }
 }
-function limitText($text, $limit){
-    $limited_text = trim(substr($text, 0, $limit));
-    if ($limited_text != $text){
-	$limited_text .= '...';
+function limitText($text, $limit){ // Cut text off beyond specified character limit
+    $limited_text = trim(substr($text, 0, $limit)); // Trim the text according to the passed limit. Use trim to remove trailing spaces
+    if ($limited_text != $text){ // If the trimmed text is different to the original (it was actually trimmed)
+        $limited_text .= '...'; // Add an ellipsis so it's clear that it's been trimmed
     }
     return $limited_text;
 }
-function renderArticleAsHTML($article, $limit=128){
+function renderArticleAsHTML($article, $limit=128){ // Render an article in JSON format as HTML with a given character limit
     if ($article != null){
-	echo "<article>"; // Use HTML <article> tags for SEO
-	$link = '?article='.getHandleByLink($article['link']);
-	echo "<h2><a href='{$link}'>{$article['title']}</a></h2>";
-	$date = date('D M Y - H:i',strtotime($article['date'])); // Format the date into a nicer format
-	echo "<time datetime='{$article['date']}' pubdate='pubdate'>{$date}</time>"; // Use HTML <time> tag for SEO
-	$description = limitText($article['description'], $limit);
-	echo "<p>{$description}</p>";
-	echo "</article>";
+        echo "<article>"; // Use HTML <article> tags for SEO
+        $link = '?article='.getHandleByLink($article['link']); // Generate dynamic link to the full article (unlimited) view
+        echo "<h2><a href='{$link}'>{$article['title']}</a></h2>"; // Render as HTML link using the title for the anchor text
+        $date = date('D M Y - H:i',strtotime($article['date'])); // Format the date into a nicer format
+        echo "<time datetime='{$article['date']}' pubdate='pubdate'>{$date}</time>"; // Use HTML <time> tag for SEO
+        $description = limitText($article['description'], $limit); // Generate the body (description) of the article, limited to $limit characters long
+        echo "<p>{$description}</p>"; // Render the post body (description) as an HTML paragraph
+        echo "</article>";
     } else {
-	echo "<h2>That article doesn't exist :(</h2><a href='?'>Go home</a>";
+        echo "<h2>That article doesn't exist :(</h2><a href='?'>Go home</a>"; // If the article doesn't exist (null passed instead of JSON)
     }
 }
-function renderFeedAsHTML($article=null){
-    $feed = getFeedAsJSON();
-    echo "<h1>{$feed['title'][0][1]}</h1>";
+function renderFeedAsHTML($article=null){ // Render all articles in feed or just one article if passed
+    $feed = getFeedAsJSON(); // Get the entire XML feed in JSON format
+    echo "<h1>{$feed['title'][0][1]}</h1>"; // Render the overall feed (channel) title as the top level heading
     echo "<a class='feed-link' href='rss.xml'>RSS Feed</a>"; // Provide a link to the raw RSS feed
     if ($article == null){ // If no article has been passed
-	foreach ($feed['articles'] as $article){ // Render every article in the feed
-	    renderArticleAsHTML($article);
-	}
+        foreach ($feed['articles'] as $article){ // Render every article in the feed
+            renderArticleAsHTML($article);
+        }
     } else {
-	renderArticleAsHTML($article, $limit=null); // Only render the passed article
+        renderArticleAsHTML($article, $limit=null); // Only render the passed article
     }
 }
 ?>
@@ -68,12 +64,12 @@ function renderFeedAsHTML($article=null){
     <body>
 	<div class="feed-container">
 	    <?php
-	    if (isset($_GET['article']) && $_GET['article'] != ''){
+    if (isset($_GET['article']) && $_GET['article'] != ''){ // If the article has been set and the article isn't blank
 		renderFeedAsHTML(getArticleByHandle($_GET['article'])); // Render a specific article (with no character limit)
-	    } else {
-		renderFeedAsHTML(); // Render the whole feed
-	    }
-	    ?>
+    } else {
+		renderFeedAsHTML(); // Render the whole feed (if a specific article isn't set)
+    }
+?>
 	</div>
     </body>
 </html>
